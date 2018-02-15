504cb85c4b891ae37302761d5f300312b6e9ec22
Nemes Gergely <nemes.gergely96@gmail.com>
2018-02-15 11:33:31 +0100

    types

---
+++ week-01/day-4/types.ts
@@ -0,0 +1,8 @@
+
+console.log('Hello!'); //this is a string
+
+console.log(21); //this is an integer
+
+console.log(21.8);  //this is a floating point number
+
+console.log(true);  //this is a boolean
