3fd03e4d7f467de92082837d14dfaa7e51eae1d8
Nemes Gergely <nemes.gergely96@gmail.com>
2018-02-15 11:43:48 +0100

    Number Operations

---
+++ week-01/day-4/operations.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+console.log(9 + 5); // Prints 14
+console.log(7 - 8); // Prints -1
+console.log(2 * 2); // Prints 4
+console.log(3 / 1); // Prints 3
+console.log(8 / 3); // Prints 2.6666666
+console.log(7 % 5); // Prints 2
