e7d6ec4160dbc3686a52a2b13a0ce52dacf5f2ef
Nemes Gergely <nemes.gergely96@gmail.com>
2018-02-15 13:11:40 +0100

    variables

---
+++ week-01/day-4/variables.ts
@@ -0,0 +1,28 @@
+// String
+let welcome: string = 'Hello, World';
+console.log(welcome);
+
+'use strict';
+
+// Boolean
+let imAProgrammer: boolean = true;
+console.log(imAProgrammer);
+
+// Integer
+let myEnergyLevel: number = 42;
+console.log(myEnergyLevel);
+
+// Floating point number
+let pi: number = 3.141592;
+console.log(pi);
+
+// Assigning and creating a variable, (define its value)
+let nummer: number = 12;
+
+// Mutate a variable, (redefine its value)
+nummer = 23;
+
+// Any
+let dynamic: any = 5;
+dynamic = false;
+dynamic = 'something';
