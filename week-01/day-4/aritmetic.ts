f7d200f638444dca80b0652d44ee22a36adde319
Nemes Gergely <nemes.gergely96@gmail.com>
2018-02-15 13:16:36 +0100

    Arithmetic Assignment Operators

---
+++ week-01/day-4/aritmetic.ts
@@ -0,0 +1,39 @@
+'use strict';
+
+let a: number = 12;
+a = a + 7;
+console.log(a); // Prints 19
+
+let b: number = 12;
+b = b - 11;
+console.log(b); // Prints 1
+
+let c: number = 3;
+c *= 3;
+console.log(c); // Prints 9
+
+let d: number = 21;
+d /= 3;
+console.log(d); // Prints 7
+
+let e: number = 8;
+e %= 7;
+console.log(e); // Prints 1
+
+let f: number = 8;
+f++;
+console.log(f); // Prints 9
+
+let g: number = 6;
+g--;
+console.log(g); // Prints 5
+
+let h: number = 5;
+console.log(h); // Prints 5
+console.log(h++); // Prints 5
+console.log(h); // Prints 6
+
+let i: number = 8;
+console.log(i); // Prints 8
+console.log(++i); // Prints 9
+console.log(i); // Prints 9
