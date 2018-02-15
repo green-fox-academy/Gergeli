5ee292416f9fc4ed7a73d87b4c5a37aebbc5356e
Nemes Gergely <nemes.gergely96@gmail.com>
2018-02-15 11:51:45 +0100

    plusz

---
+++ week-01/day-4/plusz.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+// String and special characters
+console.log('cherry'); // Prints cherry
+console.log('yes'); // Print yes
+console.log('"Everything you can imagine is real." - Picasso' + ' if you are a lunatic');
+// Prints "Everything you can imagine is real." - Picasso if you are a lunatic
+
+// String Operators
+// Concatenation
+console.log('wind' + 'shiels'); // Prints windshield
+
+// Concat string with number
+console.log('My favorite number is: ' + 9); // Prints My favorite number is: 9
