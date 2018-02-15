88ca8ed6343cca5ba251c30bd9d56b9da93d346e
Nemes Gergely <nemes.gergely96@gmail.com>
2018-02-15 14:00:48 +0100

    basic infos about myself

---
+++ week-01/day-4/define-basic-infi.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+let myName: string = 'Nemes Gergely';
+let myAge: number = 21;
+let myHeight: number = 1.8;
+let married: boolean = false;
+
+console.log('My name is ' + myName);
+console.log('My age is ' + myAge);
+console.log('My height is ' + myHeight);
+console.log('Married ? = ' + married);
