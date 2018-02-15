470c304ee871aab77c7675b8e334a20757aac8c4
Nemes Gergely <nemes.gergely96@gmail.com>
2018-02-15 14:32:55 +0100

    cuboid

---
+++ week-01/day-4/cuboid.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+let x: number = 1.2;
+let y: number = 3.7;
+let z: number = 2.3;
+
+let surface: number = x * y * 6;
+
+console.log('The cuboid\'s surface is ' + surface + ' m2');
+
+let volume: number = x * y * z;
+
+console.log('The cuboid\'s volume is ' + volume + ' m3');
