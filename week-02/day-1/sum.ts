f5e89e0e3fe5225795298c6d67c810b1e5785fbf
Nemes Gergely <nemes.gergely96@gmail.com>
2018-02-19 12:01:13 +0100

    sum

---
+++ week-02/day-1/sum.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+// -  Write a function called `sum` that sum all the numbers until the given parameter
+// -  The function should return the result
+
+function sum(a: number, b: number){
+
+    let sum: number = a + b;
+
+    return sum;
+
+}
+console.log(sum(1, 2));
