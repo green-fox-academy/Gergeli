55090def1f9dd91a7a3266cdc38c80a091f3926e
Nemes Gergely <nemes.gergely96@gmail.com>
2018-02-19 13:16:28 +0100

    sum changed

--- week-02/day-1/sum.ts
+++ week-02/day-1/sum.ts
@@ -3,11 +3,15 @@
 // -  Write a function called `sum` that sum all the numbers until the given parameter
 // -  The function should return the result
 
-function sum(a: number, b: number){
+function sum(a: number){
 
-    let sum: number = a + b;
+    let q: number = a;
+    let b: number = a;
 
-    return sum;
+    for(let i: number = 1;i <= b; i++){
+        a = a + (q-i);
+    }
 
+    return a;
 }
-console.log(sum(1, 2));
+console.log(sum(5));
