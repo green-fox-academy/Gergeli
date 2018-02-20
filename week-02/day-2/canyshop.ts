a38b53227e8bdd932cd8ca01f65eac1b61d2d22f
Nemes Gergely <nemes.gergely96@gmail.com>
2018-02-20 15:40:21 +0100

    candyshop

---
+++ week-02/day-2/canyshop.ts
@@ -0,0 +1,30 @@
+'use strict';
+
+let shopItems: any[] = ['Cupcake', 2, 'Brownie', false];
+
+// Accidentally we added "2" and "false" to the array.
+// Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
+// No, don't just remove the items :)
+// Create a function called sweets() which takes the list as a parameter.
+// Expected output: "Cupcake", "Croissant", "Brownie", "Ice cream"
+
+
+function sweets(arr: any[]){
+
+
+    arr.map(function(value, index){
+
+        if(typeof arr[index] === 'number'){
+            arr[index] = 'Croissant';
+            return arr;
+        }else if(typeof arr[index] === 'boolean'){
+            arr[index] = 'Ice cream';
+            return arr; 
+        }
+
+        return arr;
+    });
+    return arr;
+}
+
+console.log(sweets(shopItems));
