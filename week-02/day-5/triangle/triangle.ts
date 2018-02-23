d48b43afb68fbd887d0432e3d75c7c13b7812c94
Nemes Gergely <nemes.gergely96@gmail.com>
2018-02-23 15:39:08 +0100

    Triangle code style fixt

--- week-02/day-5/triangle/triangle.ts
+++ week-02/day-5/triangle/triangle.ts
@@ -6,16 +6,12 @@ const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 580;
 
-
-function angle(lines: number, step: number, height: number){
-    //let set: number = 20;
+function angle(lines: number, step: number, height: number) {
     let s: number = step/2;
     let x: number = 0;
     let y: number = canvas.height;
-
-  for(let j: number = 0; j < lines; j++){  
-    for(let i: number = 0; i < (lines-(j)); i++){
-
+  for(let j: number = 0; j < lines; j++) {  
+    for(let i: number = 0; i < (lines-(j)); i++) {
       ctx.beginPath();
       ctx.lineWidth = 1;
       ctx.strokeStyle = 'black';
@@ -24,7 +20,6 @@ function angle(lines: number, step: number, height: number){
       ctx.lineTo(x -= s, y -= (height));
       ctx.lineTo(x -= s, y += (height));
       ctx.stroke();
-
       x -= i*step;
       y = canvas.height;
     }
