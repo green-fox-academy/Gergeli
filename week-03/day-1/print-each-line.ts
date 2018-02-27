006d48053f4150ecf56ffa9828b1a6d8f329a823
Nemes Gergely <nemes.gergely96@gmail.com>
2018-02-27 11:55:41 +0100

    error handeling fixet

--- week-03/day-1/print-each-line.ts
+++ week-03/day-1/print-each-line.ts
@@ -5,10 +5,9 @@
 // If the program is unable to read the file (for example it does not exists),
 // then it should print an error message like: 'Unable to read file: my-file.txt'
 let fs = require('fs');
-let file = fs.readFileSync('./my-file.txt', 'utf-8');
 
 try {
-    console.log(file);
+    console.log(fs.readFileSync('./my-file.txt', 'utf-8'));
 } catch (error) {
     console.log('Unable to read file: my-file.txt');
 }
