bc737e9fa5fe6583df25cf7ba8b039550fdbb051
Nemes Gergely <nemes.gergely96@gmail.com>
2018-02-28 10:43:00 +0100

    table pinter in progress

---
+++ week-02/day-4/table-printer.ts
@@ -0,0 +1,41 @@
+'use strict';
+// Create a function that prints the ingredient list of dictionaries to the console in the following format:
+//
+// +--------------------+---------------+----------+
+// | Ingredient         | Needs cooling | In stock |
+// +--------------------+---------------+----------+
+// | vodka              | Yes           | 1        |
+// | coffee_liqueur     | Yes           | -        |
+// | fresh_cream        | Yes           | 1        |
+// | captain_morgan_rum | Yes           | 2        |
+// | mint_leaves        | No            | -        |
+// +--------------------+---------------+----------+
+//
+// OPTIONAL
+// The frist columns should be automatically as wide as the longest key
+let ingredients: any[] = [
+  { name: 'vodka', in_stock: 1, needs_cooling: true },
+  { name: 'coffee_liqueur', in_stock: 0, needs_cooling: true },
+  { name: 'fresh_cream', in_stock: 1, needs_cooling: true },
+  { name: 'captain_morgan_rum', in_stock: 2, needs_cooling: true },
+  { name: 'mint_leaves', in_stock: 0, needs_cooling: false },
+  { name: 'sugar', in_stock: 0, needs_cooling: false },
+  { name: 'lime juice', in_stock: 0, needs_cooling: true },
+  { name: 'soda', in_stock: 0, needs_cooling: true }
+];
+
+function raw(arr: any[]){
+    let long: string = '';
+
+    for(let i: number = 0; i < arr.length; i++){
+        
+    }
+
+}
+
+
+function list(arr: any[]){
+
+
+
+}
