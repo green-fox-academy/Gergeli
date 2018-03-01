ac97994e4875debd989d52333603aff816b8ea4d
Nemes Gergely <nemes.gergely96@gmail.com>
2018-03-01 10:49:37 +0100

    Styleguide

--- week-03/day-4/sum-digit.ts
+++ week-03/day-4/sum-digit.ts
@@ -5,14 +5,12 @@
 // divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
 
 function somOfDigits(n: number) {
-  let a: number = Math.floor(n);
-  let b: number = a % 10;
-  if(n < 1){
+  let b: number = Math.floor(n % 10);
+  if(n < 1) {
     return 0;
   }else{
-    b += somOfDigits((n /10));
+   return b += somOfDigits((n /10));
   }
-  return b;
 }
 
-console.log(somOfDigits(9353));
+console.log(somOfDigits(59158));
